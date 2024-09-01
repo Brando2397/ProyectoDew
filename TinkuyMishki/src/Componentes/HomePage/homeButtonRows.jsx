3eee344e573068eb7ef3ac4fc8fb69da579f7b75
i202409263 <i202409263@cibertec.edu.pe>
2024-09-01 04:36:26 +0000

    homePage

---
+++ TinkuyMishki/src/Componentes/HomePage/homeButtonRows.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Fragment } from "react";
+import '../styles/homeButtonRows.css';
+
+function RowButtons() {
+    return(
+        <>
+            <div class="rows-main">
+            <a href="" class="button" id="button1">PLATOS CRIOLLOS</a>
+            <a href="" class="button" id="button2">PLATOS MARINOS</a>
+            <a href="" class="button" id="button3">POLLOS  Y PARRILLAS</a>
+           </div>
+        </>
+    )
+};
+
+export default RowButtons;
